158aefec3c33310a5077db0e3e50e3e801ffe834
danielU2grow <ndong.phanuel@uni2growcameroun.com>
2025-09-19 10:02:51 +0100

    bug(OH2-102): remove setlock null en supplier controller

--- src/main/java/org/isf/supplier/rest/SupplierController.java
+++ src/main/java/org/isf/supplier/rest/SupplierController.java
@@ -78,7 +78,6 @@ public class SupplierController {
 	public SupplierDTO saveSupplier(@RequestBody @Valid SupplierDTO supplierDTO) throws OHServiceException {
 		LOGGER.info("Saving a new supplier...");
 		try {
-			supplierDTO.setSupId(null);
 			Supplier newSupplier = manager.saveOrUpdate(mapper.map2Model(supplierDTO));
 			LOGGER.info("Supplier saved successfully.");
 			return mapper.map2DTO(newSupplier);
