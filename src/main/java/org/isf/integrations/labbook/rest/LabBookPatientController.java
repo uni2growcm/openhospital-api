b0f003613660c6dde01631e1b79a809d072e0548
duval2dev <duval_donfack@uni2growcameroun.com>
2026-04-10 11:15:38 +0100

    (OH2-304) Refactor code

--- src/main/java/org/isf/integrations/labbook/rest/LabBookPatientController.java
+++ src/main/java/org/isf/integrations/labbook/rest/LabBookPatientController.java
@@ -4,26 +4,28 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.isf.integrations.labbook.models.PatientAnalysisResponse;
 import org.isf.integrations.labbook.services.PatientSyncService;
+import org.isf.utils.exception.OHException;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @Tag(name = "Labbook")
 @SecurityRequirement(name = "bearerAuth")
 @RequestMapping(value = "/labbook", produces = MediaType.APPLICATION_JSON_VALUE)
-public class LabBookController {
+public class LabBookPatientController {
 
 	private final PatientSyncService patientSyncService;
 
-	public LabBookController(PatientSyncService patientSyncService) {
+	public LabBookPatientController(PatientSyncService patientSyncService) {
 		this.patientSyncService = patientSyncService;
 	}
 
 	@GetMapping("/patients/{id}/analysis")
 	@PreAuthorize("hasRole('ADMIN') or hasAuthority('exams.read')")
-	public ResponseEntity<PatientAnalysisResponse> getPatientAnalysis(@PathVariable Integer id) {
-		return ResponseEntity.ok(patientSyncService.getPatientAnalysis(id));
+	public List<PatientAnalysisResponse> getPatientAnalysis(@PathVariable Integer id) throws OHException {
+		return patientSyncService.getPatientAnalysis(id);
 	}
 }
